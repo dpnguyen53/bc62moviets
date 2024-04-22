a8cb3d4ed0d0c3459db47248c793b1ded17825bc
dpnguyen53 <dpnguyen53@gmail.com>
2024-04-22 19:08:05 +0700

    buoi 11

---
+++ src/pages/AdminTemplate/index.tsx
@@ -0,0 +1,7 @@
+export default function AdminTemplate() {
+  return (
+    <div>
+      <h3>AdminTemplate</h3>
+    </div>
+  );
+}
---
+++ src/pages/HomeTemplate/index.tsx
@@ -0,0 +1,7 @@
+export default function HomeTemplate() {
+  return (
+    <div>
+      <h3>HomeTemplate</h3>
+    </div>
+  );
+}
